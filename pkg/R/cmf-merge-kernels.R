71d15f2ebe745e83ec78001332c8eb9baf14d696
eshklyar <eshklyar@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-01-17 20:19:25 +0000

    comment out  source() lines in all .R files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cmf@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/cmf-merge-kernels.R
@@ -0,0 +1,14 @@
+# Merging files with computed kernels
+
+cmf_merge_kernels <- function(kernels_train_fname_list, kernels_train_merged_fname)
+{
+  kernels_merged <- list()
+  for (kernels_fname in kernels_fname_list) {
+    load(kernels_fname)
+	field_names <- names(kernels)
+	for (el in field_names) kernels_merged[[el]] <- kernels[[el]]
+  }
+  kernels <- kernels_merged
+  save(kernels, file=kernels_merged_fname)
+}
+
